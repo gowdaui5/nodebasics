efaa97e47e8086951deaec2285cd2897ca969d15
gowdaui5 <srinivas.ramesha@gmail.com>
2021-07-21 17:43:06 +0530

    Server and html page configurations

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 // const txtIn = fs.readFileSync('./txt/input.txt','utf-8');
 // console.log(txtIn);
-
 // const txtOut = `This is what we know about avacado:  ${txtIn}.\Created on ${Date.now()}`;
 // fs.writeFileSync('./txt/output.txt',txtOut);
 // console.log('File Written');
@@ -12,16 +12,45 @@ const http = require('http');
 //         console.log(data1);
 //     })
 // })
+const updateEachCard = (tempCard, product) => {
+    let output = tempCard.replace(/{%PRODUCT_NAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+};
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
+const tempCards = fs.readFileSync(`${__dirname}/templates/template-cards.html`, 'utf-8')
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
+    const { query, pathname } = url.parse(req.url, true);
     const pathName = req.url;
-    if (pathName === '/') {
-        res.end('This is initial page');
-    } else if (pathName === '/product') {
-        res.end('this is product')
-    } else if (pathName === '/api') {
+
+    //overview page
+    if (pathname === '/' || pathname === '/overview') {
+        const cardsHTML = dataObj.map(el => updateEachCard(tempCards, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHTML);
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(output);
+        //product page
+    } else if (pathname === '/product') {
+        const product = dataObj[query.id];
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        const output = updateEachCard(tempProduct, product);
+        res.end(output);
+
+        //API
+    } else if (pathname === '/api') {
         res.writeHead(200, { 'Content-Type': 'application/json' })
         res.end(data)
     } else {
